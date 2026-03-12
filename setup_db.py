7ff541396ed752fd4d4a3cf25a6e94088e33a196
heavy-metal-tech <wmatsuro@hotmail.com>
2026-03-12 00:47:08 -0300

    Refactor setup_db.py to ensure instance directory exists

--- setup_db.py
+++ setup_db.py
@@ -1,14 +1,24 @@
+import os
 from app import app, db, User
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+instance_path = os.path.join(basedir, 'instance')
+
 with app.app_context():
+    # Garante que a pasta instance existe
+    if not os.path.exists(instance_path):
+        os.makedirs(instance_path)
+    
     db.create_all()
-    # Criar Chefe
+    
+    # Adiciona usuários se não existirem
     if not User.query.filter_by(username='chefe').first():
         admin = User(username='chefe', password='minipa_admin_2026', cargo='admin')
         db.session.add(admin)
-    # Criar Seu Acesso
+    
     if not User.query.filter_by(username='will').first():
         will = User(username='will', password='123', cargo='tecnico')
         db.session.add(will)
+    
     db.session.commit()
-    print("Usuários de teste criados!")
+    print("Banco de dados e usuários criados com sucesso!")
