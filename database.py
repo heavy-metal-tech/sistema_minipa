3555cb084377ab4dd50ac1617fe234b605a38136
heavy-metal-tech <wmatsuro@hotmail.com>
2026-03-13 00:27:00 -0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    nome_completo = db.Column(db.String(100))
+    is_admin = db.Column(db.Boolean, default=False)
+
+class OrdemServico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cliente = db.Column(db.String(100))
+    equipamento = db.Column(db.String(100), nullable=False)
+    serie = db.Column(db.String(50), nullable=False)
+    nota_fiscal = db.Column(db.String(50))
+    garantia = db.Column(db.String(10))
+    valor = db.Column(db.String(20))
+    defeito = db.Column(db.Text)
+    tecnico = db.Column(db.String(100))
