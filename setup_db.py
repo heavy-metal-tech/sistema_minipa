cad540e320af41ac12e8f812c02a05e406708698
heavy-metal-tech <wmatsuro@hotmail.com>
2026-03-13 00:27:55 -0300

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,22 +1,15 @@
-from app import app, db, User, OrdemServico
+from app import app
+from database import db, User, OrdemServico
 from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    print("Limpando banco de dados para eliminar erros 405/500...")
+    print("Executando Colapso Quântico: Limpando tudo...")
     db.drop_all()
     db.create_all()
     
-    # Criar você como Admin
-    pw = generate_password_hash('123', method='pbkdf2:sha256')
-    admin = User(username='will', password=pw, nome_completo='Will - Admin')
+    admin_pw = generate_password_hash('123', method='pbkdf2:sha256')
+    admin = User(username='will', password=admin_pw, nome_completo='Will - Master', is_admin=True)
     db.session.add(admin)
     
-    # OS de teste para confirmar que o Dashboard carrega corretamente
-    teste = OrdemServico(
-        cliente="Minipa Matriz", equipamento="ET-2042E", 
-        serie="SN-12345", garantia="Sim", valor="0.00", tecnico="Will"
-    )
-    db.session.add(teste)
-    
     db.session.commit()
-    print("--- SISTEMA SINCRONIZADO: USE 'will' E '123' ---")
+    print("SISTEMA RESETADO E LIMPO!")
