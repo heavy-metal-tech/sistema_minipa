bb7631d24c9d9e9f03e1d7337196dc5acb77ac10
heavy-metal-tech <wmatsuro@hotmail.com>
2026-03-12 22:05:42 -0300

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,24 +1,20 @@
-import os
-from app import app, db, User
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-instance_path = os.path.join(basedir, 'instance')
+from app import app, db, User, Estoque
 
 with app.app_context():
-    # Garante que a pasta instance existe
-    if not os.path.exists(instance_path):
-        os.makedirs(instance_path)
-    
     db.create_all()
     
-    # Adiciona usuários se não existirem
-    if not User.query.filter_by(username='chefe').first():
-        admin = User(username='chefe', password='minipa_admin_2026', cargo='admin')
-        db.session.add(admin)
-    
+    # Usuário Will
     if not User.query.filter_by(username='will').first():
-        will = User(username='will', password='123', cargo='tecnico')
-        db.session.add(will)
+        db.session.add(User(username='will', password='123'))
+    
+    # Itens de Estoque para demonstração
+    if not Estoque.query.first():
+        itens = [
+            Estoque(item='Fusível Cerâmico 10A', quantidade=2, minimo=5),
+            Estoque(item='Pilha 9V Alcalina', quantidade=20, minimo=5),
+            Estoque(item='Garra Jacaré Preta', quantidade=3, minimo=10)
+        ]
+        db.session.add_all(itens)
     
     db.session.commit()
-    print("Banco de dados e usuários criados com sucesso!")
+    print("Sistema pronto e estoque inicializado!")
